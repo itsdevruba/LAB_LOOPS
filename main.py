9573c988cd4046e23d5ea729b5e49b73ae36e520
itsdevruba <rubaaldawssari@gmail.com>
2026-09-09 18:57:09 +0300

    the first Q done

---
+++ main.py
@@ -0,0 +1,10 @@
+
+
+print ("_"*20)
+
+qu = "what is the product of 7 * 24 ?"
+while input(qu) != "168":
+    print("Your Answer is wrong try again..")
+else:
+    print("You answered this Question correctly")
+
